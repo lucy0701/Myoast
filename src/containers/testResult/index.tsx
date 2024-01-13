2be1f366c5ec85e4594ec3651a8c37c886fc446b
Crystal P <sj8871@gmail.com>
2024-01-14 00:03:10 +0900

    Building
    test

--- src/containers/testResult/index.tsx
+++ src/containers/testResult/index.tsx
@@ -1,33 +1,27 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import { TYPE_BOTTOM_BTN } from '@/constants/constant';
-import { useComment } from '@/hooks/useComment';
-import { useLike } from '@/hooks/useLike';
-import { Test } from '@/types/test';
 
 import Button from '@/components/common/Button';
 import { ResultCountBtn } from '@/components/layout/CountBtn';
 import AddComment from '@/components/layout/AddComment';
-import Comments from '@/components/layout/Comments';
 import styles from './index.module.css';
 
 // post 필요 데이터 연결 해야함
 export default function TestResult() {
-    return (
-      <div className={styles.wrap}>
-        <div className={styles.resultImg}>
-          <img src="/images/test/card.jpeg" alt="test" />
-        </div>
-        <h2 className={styles.tsetTitle}>결과 제목</h2>
-        <div className={styles.textBox}>
-          <p>결과 내용</p>
-        </div>
-        <ResultCountBtn likeCount={0}></ResultCountBtn>
-        <AddComment commentCount={0} />
-        {/* <Comments commentList={commentList} /> */}
-        <Button skin={TYPE_BOTTOM_BTN}>테스트 결과 공유하기</Button>
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.resultImg}>
+        <img src="/images/test/card.jpeg" alt="test" />
       </div>
-    );
-  }
+      <h2 className={styles.tsetTitle}>결과 제목</h2>
+      <div className={styles.textBox}>
+        <p>결과 내용</p>
+      </div>
+      <ResultCountBtn likeCount={0}></ResultCountBtn>
+      <AddComment commentCount={0} />
+      {/* <Comments commentList={commentList} /> */}
+      <Button skin={TYPE_BOTTOM_BTN}>테스트 결과 공유하기</Button>
+    </div>
+  );
+}
