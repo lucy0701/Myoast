0c0219c8b11c7313cd2900238a614d0c8c769c09
Crystal P <sj8871@gmail.com>
2024-01-14 23:16:36 +0900

    navigation
    13부터는.. 된다... navigation...

--- src/app/latest/page.tsx
+++ src/app/latest/page.tsx
@@ -4,8 +4,7 @@ import TestLatest from '@/containers/testLatest';
 import { getLatestListAPI } from '@/services/test';
 
 export default async function Page() {
-  const testLatestData = await getLatestListAPI('5')
-    .then((res) => res.data.testCoverDTOList);
+  const testLatestData = await getLatestListAPI('5').then((res) => res.data.testCoverDTOList);
   if (!testLatestData) return notFound;
   return <TestLatest testLatestData={testLatestData} />;
 }
