822f5546e68ad1915b50fb90400c34df2e75a076
Crystal P <sj8871@gmail.com>
2024-01-25 22:00:42 +0900

    -
    -

--- src/app/result/[testId]/page.tsx
+++ src/app/result/[testId]/page.tsx
@@ -1,29 +1,5 @@
 import TestResult from "@/containers/testResult";
-import { DOMAIN } from '@/constants/constant';
 
-export async function generatMetadata() {
-  const url = `${DOMAIN}/main`;
-  const title = '몽빗[MongBit]';
-  let description = 'MBTI 심리테스트 공작소';
-  let imageUrl = `${DOMAIN}/common/opengraph-image`;
-
-  return {
-    title,
-    description,
-    openGraph: {
-      description,
-      type: 'website',
-      title,
-      url,
-      images: [
-        {
-          url: imageUrl,
-          alt: 'og_image',
-        },
-      ],
-    },
-  };
-}
 export default function page() {
     return <TestResult/>;
 }
--- src/app/test/play/[testId]/page.tsx
+++ src/app/test/play/[testId]/page.tsx
@@ -2,31 +2,6 @@ import { notFound } from 'next/navigation';
 
 import TestPlay from '@/containers/testPlay';
 import { getTestAPI } from '@/services/test';
-import { DOMAIN } from '@/constants/constant';
-
-export async function generatMetadata() {
-  const url = `${DOMAIN}/main`;
-  const title = '몽빗[MongBit]';
-  let description = 'MBTI 심리테스트 공작소';
-  let imageUrl = `${DOMAIN}/common/opengraph-image`;
-
-  return {
-    title,
-    description,
-    openGraph: {
-      description,
-      type: 'website',
-      title,
-      url,
-      images: [
-        {
-          url: imageUrl,
-          alt: 'og_image',
-        },
-      ],
-    },
-  };
-}
 
 interface Props {
   params: {
