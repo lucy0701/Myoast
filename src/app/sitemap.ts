b38711070d683645ff597b25668e3132f9c8201b
Crystal P <sj8871@gmail.com>
2024-01-26 07:29:59 +0900

    sitemap update
    -

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,24 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mongbit-fe.vercel.app/mypage',
+      url: 'https://mongbit-fe.vercel.app/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mongbit-fe.vercel.app/list',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mongbit-fe.vercel.app/latest',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mongbit-fe.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mongbit-fe.vercel.app/devinfo',
+      lastModified: new Date(),
     },
   ];
 }
