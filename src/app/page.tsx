2e82c231f21cfb83230c0e13d277d0256cfe2d3a
Crystal P <sj8871@gmail.com>
2024-01-29 00:06:28 +0900

    -
    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { TestCover, TestCoverResponse } from '@/types/test';
 import Main from '../containers/main';
 
 export async function generateMetadata() {
-  const url = `${DOMAIN}`;
+  const url = `${DOMAIN}/`;
   const title = '묘스트 [무료 | MBTI 심리테스트]';
   let description = '묘스트의 MBTI 심리테스트';
   let imageUrl = `${DOMAIN}/main/opengraph-image`;
