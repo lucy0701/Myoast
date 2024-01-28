b60cbcbbddf27a20d6140d6caf1ab1bec21d1eae
Crystal P <sj8871@gmail.com>
2024-01-28 21:01:01 +0900

    이미지 변경
    -

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,23 +4,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://myoast.vercel.app/',
-      lastModified: new Date(),
     },
     {
       url: 'https://myoast.vercel.app/list',
-      lastModified: new Date(),
     },
     {
       url: 'https://myoast.vercel.app/latest',
-      lastModified: new Date(),
     },
     {
       url: 'https://myoast.vercel.app/about',
-      lastModified: new Date(),
     },
     {
       url: 'https://myoast.vercel.app/devinfo',
-      lastModified: new Date(),
     },
   ];
 }
