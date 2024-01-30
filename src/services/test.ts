cb8337ef0de5d072906ddf285707344c4637c988
Crystal P <sj8871@gmail.com>
2024-01-30 17:43:31 +0900

    add rss
    -

--- src/services/test.ts
+++ src/services/test.ts
@@ -1,8 +1,10 @@
 import { apiBe } from '@/services';
-import { Test } from '@/types/test';
+import { Test, TestCover } from '@/types/test';
 import { getHeaders } from '@/utils/util';
 
 const headers = getHeaders();
+
+export const getTestListAPI = () => apiBe<TestCover[]>(`v1/tests`, { headers });
 
 export const getTestAPI = (testid: string) => apiBe<Test>(`v1/tests/test/${testid}`, { headers });
 
@@ -10,6 +12,8 @@ export const getTestAPI = (testid: string) => apiBe<Test>(`v1/tests/test/${testi
 export const postTestResultAPI = (testId: string, score: number[]) =>
   apiBe.post(`/v1/member-test-result/${testId}`, score, { headers });
 
-export const postMemberTestResultAPI = (testId: string, memberId:string, score: number[]) => apiBe.post(`/v1/member-test-result/${testId}/${memberId}`, score, { headers });
+export const postMemberTestResultAPI = (testId: string, memberId: string, score: number[]) =>
+  apiBe.post(`/v1/member-test-result/${testId}/${memberId}`, score, { headers });
 
-export const getTestResultAPI = (testId: string, testResultId: string) => apiBe(`v1/tests/test/test-result/${testId}/${testResultId}`,{headers});
+export const getTestResultAPI = (testId: string, testResultId: string) =>
+  apiBe(`v1/tests/test/test-result/${testId}/${testResultId}`, { headers });
